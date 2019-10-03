c65a85d17a118dcd3008e8dbb75c9ce4d6ffdf14
jmartorell <martorelljosep@gmail.com>
2019-10-03 21:20:44 +0200

    refactoring

--- scrapybot/spiders/spider_1.py
+++ scrapybot/spiders/spider_1.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
+# url to be scratched: https://sfbay.craigslist.org/search/sfc/sof
+# This spider is programmed to track information about software job offers in San Francisco Bay.
+# standalone commands:
+# username@hostname$ scrapy runspider spider_1.py -o craigs.csv -t csv
 
-from scrapy.spiders import CrawlSpider, Rule # just "Rule" is to implement scrapy rules
+from scrapy.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
-from scrapy.linkextractors import LinkExtractor # to implement scrapy rules
+from scrapy.linkextractors import LinkExtractor
 from scrapybot.items import ScrapybotItem
 
 
-class MySpider(CrawlSpider):
-    name = 'craigsCrawler'
+class MySpider1(CrawlSpider):
+    name = 'sanFranciscoCrawler'
     allowed_domains = ['sfbay.craigslist.org']
-    start_urls = ['http://sfbay.craigslist.org/search/npo']
+    start_urls = ['https://sfbay.craigslist.org/search/sfc/sof']
 
-    # implementing scrapy rules
-#    rules = (
-#        Rule(LinkExtractor(allow=(),
-#             restrict_xpaths=('//a[@class="button next"]',)),
-#             callback="parse_items", follow=True),
-#    )
+# implementing rules. Instead "parse_items" callbacks with "parse" should invoke directly scrapybot engine.
+# parameter follow is set to true to keep the same rules for the next items.
 
-    def parse(self, response):
+    rules = (
+        Rule(LinkExtractor(allow=(),
+             restrict_xpaths=('//li[@class="result-row"]',)),
+             callback="parse_items", follow=True),
+    )
+
+    @staticmethod
+    def parse_items(response):
         hxs = Selector(response)
         titles = hxs.xpath('//ul[@class="rows"]')
         items = []
         for title in titles:
             item = ScrapybotItem()
-            item['title'] = title.select('li/p/a/text()').extract()
-            item['link'] = title.xpath('li[@class="result-row"]/a/@href').extract()
+            item['url'] = response.url
+            item['job_title'] = title.select('//p/a/text()').extract()
+            item['link'] = title.xpath('//li[@class="result-row"]/p/a/@href').extract()
             items.append(item)
         return items
 
-# $ scrapy runspider spider_1.py -o craigs.csv -t csv
+
