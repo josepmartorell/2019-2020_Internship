b50d3564d1e47e85c50de2fb4ff82a1f70d2d760
jmartorell <martorelljosep@gmail.com>
2019-11-30 18:02:58 +0100

    spath researchs

--- travelbot/travelbot/pipelines.py
+++ travelbot/travelbot/pipelines.py
@@ -4,6 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+#
+# Item Pipeline can be used to validate scraped data, check duplicate data,
+# or insert the data into databases such as Mysql, PostgreSQL or MongoDB.
+import json
+
 from scrapy.exceptions import DropItem
 
 
@@ -12,7 +17,7 @@ class TravelbotPipeline(object):
         return item
 
 
-class TravelbotDuplicatesPipeline(object):
+class DuplicatesPipeline(object):
 
     def __init__(self):
         self.ids_seen = set()
@@ -23,3 +28,31 @@ class TravelbotDuplicatesPipeline(object):
         else:
             self.ids_seen.add(item['id'])
             return item
+
+
+class PricePipeline(object):
+
+    vat_factor = 1.15
+
+    def process_item(self, item, spider):
+        if item.get('price'):
+            if item.get('price_excludes_vat'):
+                item['price'] = item['price'] * self.vat_factor
+            return item
+        else:
+            raise DropItem("Missing price in %s" % item)
+
+
+#class JsonWriterPipeline(object):
+
+#    def open_spider(self, spider):
+#        self.file = open('items.jl', 'w')
+
+#    def close_spider(self, spider):
+#        self.file.close()
+
+#    def process_item(self, item, spider):
+#        line = json.dumps(dict(item)) + "\n"
+#        self.file.write(line)
+#        return item
+
--- travelbot/travelbot/spiders/travelbot.py
+++ travelbot/travelbot/spiders/travelbot.py
@@ -10,22 +10,36 @@ class ToScrapeSpiderXPath(scrapy.Spider):
     ]
 
     def parse(self, response):
+        #Remove XML namespaces
+        response.selector.remove_namespaces()
         for element in response.xpath('//ul[@id="owl-carousel-caribe"]'):
             yield {
-#               TODO: xpath suit to investigate domain topology...
+#               TODO:
+#                ...the main functions...
+#                The selectors have 4 methods:
+#                xpath (): Returns a list of selectors, each representing the nodes by the xpath expression passed as an argument.
+#                css (): Returns a list of selectors, each representing the nodes selected by the css () expression passed as an argument.
+#                extract (): Returns a string (unicode) with the selected data (extract.first() only extracts the first date of the list).
+#                re (): Returns a list of extracted strings when applying a regular expression passed as an argument.
+#                ...check domain topology with xpath...
 #                'all-text-nodes': element.xpath('string(string(//body))').extract_first()
 #                'document-nodes-order': element.xpath('following:: *').extract_first()
 #                'node_test': element.xpath('//li/div/div[4]/p/node()').extract_first()
 #                'positional_predicate': element.xpath('( //body//div ) [1]').extract()
 #                'position_ranges': element.xpath('//body//div//li[position()>1 and position()<last()]').extract()
-#                'location_path': element.xpath('//li/div/div[4]/p/text()').extract()
 #                'top-caribe-star': element.xpath('//footer//div[3]/div/div/ul/li[1]/a/text()').extract()
-                'paragraphs': element.xpath('count(//p)').extract_first(),
-                'attributes': element.xpath('count(//@*)').extract_first(),
-                'destination': element.xpath('//h3/a/text()').extract(),
-                'top-caribe': element.xpath('//footer//div[3]//a/text()').extract()
+#                'location_path': element.xpath('//li/div/div[4]/p/text()').extract(), # sorted list, mixed targets!
+#                'destination': element.xpath('//h3/a/text()').extract(),
+#                'top-caribe': element.xpath('//footer//div[3]//a/text()').extract(),
+#                ...check quantitative rates with count() function...
+#                'paragraphs': element.xpath('count(//p)').extract_first(),
+#                'attributes': element.xpath('count(//@*)').extract_first(),
+#                ...paths found...
+                'caribe_pack_destination': element.xpath('//li/div/h2/a/text()').extract_first(),
+                'price': element.xpath('//li/div/div[4]/p[@class="frnv-price"]/text()').extract_first()
             }
 
         next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
+
