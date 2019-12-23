1ca446e06d90344b9a1df379e38844c4dc1dc9d6
¨josepmartorell¨ <martorelljosep@gmail.com>
2019-12-23 23:34:26 +0100

    upload

--- business&travel/main.py
+++ business&travel/main.py
@@ -3,26 +3,16 @@
 @author: jmartorell
 """
 
+import time
 from selenium import webdriver
-from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 url = "https://pro.w2m.travel"
-
-# options = Options()
-# options.add_argument("window-size=1400,600")
-
-# ua = UserAgent()
-# user_agent = ua.random
-
-# print(user_agent)
-
-# options.add_argument(f'user-agent={user_agent}')
-# browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver", options=options)
 
 browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 browser.get(url)
@@ -43,14 +33,22 @@ element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((
 element = browser.find_element_by_xpath(
     '//*[@id="hotel-searcher-_ctl1__ctl1__ctl1_pageBody_pageBody_searcher__ctl0_ctlZoneSelector-input"]')
 element.clear()
-element.send_keys("London")
-# element.click()
 # TODO
+element.clear()
+element.send_keys("london")
+
+actions = ActionChains(browser)
+for _ in range(3):
+    actions.send_keys(Keys.ARROW_DOWN).perform()
+    time.sleep(1)
+
+element.send_keys(Keys.RETURN).click()
+
 login_attempt = element.find_element_by_xpath("/html/body/form/div[1]/header/div[2]/div/div/div[2]/div[2]/button")
-login_attempt.click()
+login_attempt.submit()
 
 
 print(browser.current_url)  # TRACER
-import time
-time.sleep(20)
+
+time.sleep(6)
 browser.quit()
