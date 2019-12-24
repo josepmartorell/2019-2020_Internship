6eadcd96c2774164cf26f17aa58b8e6e3daabab5
¨josepmartorell¨ <martorelljosep@gmail.com>
2019-12-24 23:04:33 +0100

    upload

--- business&travel/main.py
+++ business&travel/main.py
@@ -11,44 +11,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-
+# url access
 url = "https://pro.w2m.travel"
-
 browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 browser.get(url)
 
+# logging
 inputElement = browser.find_element_by_id("email")
 inputElement.clear()
 inputElement.send_keys("business.travel")
 inputElement = browser.find_element_by_id("password")
 inputElement.clear()
 inputElement.send_keys("Busi2016")
-
 inputElement.submit()
 
+# wait to load the search engine
 element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((
     By.XPATH,
     '//*[@id="hotel-searcher-_ctl1__ctl1__ctl1_pageBody_pageBody_searcher__ctl0_ctlZoneSelector-input"]'))).click()
-
 element = browser.find_element_by_xpath(
     '//*[@id="hotel-searcher-_ctl1__ctl1__ctl1_pageBody_pageBody_searcher__ctl0_ctlZoneSelector-input"]')
 element.clear()
-# TODO
-element.clear()
+
+# check access
+assert "Hotels | W2M" in browser.title
+print(browser.current_url)
+
+# enter data in input field
 element.send_keys("london")
 
+# TODO:
+# drop-down item selection
 actions = ActionChains(browser)
 for _ in range(3):
     actions.send_keys(Keys.ARROW_DOWN).perform()
     time.sleep(1)
 
-element.send_keys(Keys.RETURN).click()
-
+# press button
 login_attempt = element.find_element_by_xpath("/html/body/form/div[1]/header/div[2]/div/div/div[2]/div[2]/button")
 login_attempt.submit()
 
 
-print(browser.current_url)  # TRACER
-
+# close the browser
 time.sleep(6)
 browser.quit()
+
+# FIXME:
+# when selecting from the drop down before moving three items down
+# you have already slowed down two items !!??!?
+# or does not select the item or does not press the button, or both...
+# tentative solution: build a list with the itmes to then try on it in the for cycle
+# you can create a module to do it
--- nautalia/main.py
+++ nautalia/main.py
@@ -3,63 +3,43 @@
 @author: jmartorell
 """
 
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
+# url access
 url = "https://www.nautaliaviajes.com/"
-
-
 driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 driver.get(url)
 
-inputElement = driver.find_element_by_id("hoteles_destino")
-inputElement.clear()
-inputElement.send_keys("london")
+# page checking
+assert "NAUTALIA VIAJES" in driver.title
+print(driver.current_url)
 
-"""
-inputElement = driver.find_element_by_id("hoteles_start_date")
-inputElement.clear()
-inputElement.send_keys("")
-elementos = driver.find_elements_by_class_name("calendarCellOpen")
-while True:
 
-        if elementos:
-            driver.find_element_by_class_name("calendarCellOpen").click()
-            driver.find_element_by_id("ctl00_ContentPlaceHolder1_acc_Calendario1_repFasce_ctl01_btnConferma").click() #confirm button
-        else:
+# cookies policy alert
+alert = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((
+    By.ID,
+    'capaAvisoPoliticaCookies_superior_mensajes')))
+alert.click()
 
-            driver.find_element_by_xpath("//input[@value='<']").click()  #back
-            if elementos:
-                driver.find_element_by_class_name("calendarCellOpen").click()
-                driver.find_element_by_id("ctl00_ContentPlaceHolder1_acc_Calendario1_repFasce_ctl01_btnConferma").click()
+# enter data in input field
+input = driver.find_element_by_xpath(
+    '//*[@id="hoteles_destino"]')
+input.clear()
+input.send_keys("london")
 
-            driver.find_element_by_xpath("//input[@value='>']").click() #forward
-            if elementos:
-                driver.find_element_by_class_name("calendarCellOpen").click()
-                driver.find_element_by_id("ctl00_ContentPlaceHolder1_acc_Calendario1_repFasce_ctl01_btnConferma").click()
 
-# inputElement.submit()
-
-# element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((
-#     By.XPATH,
-#     '//*[@id="hotel-searcher-_ctl1__ctl1__ctl1_pageBody_pageBody_searcher__ctl0_ctlZoneSelector-input"]'))).click()
-
-# element = browser.find_element_by_xpath(
-#     '//*[@id="hotel-searcher-_ctl1__ctl1__ctl1_pageBody_pageBody_searcher__ctl0_ctlZoneSelector-input"]')
-# element.clear()
-# element.send_keys("London")
-# element.click()
-# TODO
-# login_attempt = element.find_element_by_xpath("/html/body/form/div[1]/header/div[2]/div/div/div[2]/div[2]/button")
-# login_attempt.click()
-
-"""
-
-# print(browser.current_url)  # TRACER
-import time
-time.sleep(20)
+# close navigation session
+time.sleep(10)
 driver.quit()
+
+# FIXME:
+# raise exception_class(message, screen, stacktrace)
+# selenium.common.exceptions.ElementClickInterceptedException:
+# Message: Element <input id="hoteles_destino" class="frnv-input ui-autocomplete-input"
+# name="hoteles_destino" type="text"> is not clickable at point (206,613)
+# because another element <div id="capaAvisoPoliticaCookies_superior_mensajes"
+# class="capaAvisoPoliticaCookies_superior_mensajes"> obscures it
