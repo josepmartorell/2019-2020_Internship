c75f9d4a6607d230b3d4c770df70f4506a05be8c
josepmartorell <martorelljosep>
2020-05-20 20:49:44 +0200

    update

--- solole/test/snapshoots.py
+++ solole/test/snapshoots.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import calendar
 
-# todo Vademecum:
+# todo vademecum:
 # time, time(), localtime(), asctime(), ctime(), mktime(), altzone, sleep(), tzset(), strftime()
 # datetime, timedelta(),
 # datetime.datetime, utcnow()
@@ -38,7 +38,7 @@ now = datetime.datetime.utcnow()
 yesterday = now - datetime.timedelta(days=1)
 print("yesterday: ", yesterday)
 
-print("\n\tsnap:\n") # todo: CALCULATE
+print("\n\tsnap:\n")  # todo: CALCULATE
 today = datetime.datetime.utcnow()
 days = 6
 print("check in:  ", today)
@@ -52,10 +52,6 @@ print("a week are: ", week)
 print("\n\tsnap:\n")
 ticks = time.time()
 print("Tick number from 12:00 am, January 1, 1970:", ticks)
-
-print("\n\tsnap:\n")  # todo: STRUCT
-localtime = time.localtime(time.time())
-print("Current local time: ", localtime)
 
 print("\n\tsnap:\n")  # todo: CALENDAR
 cal = calendar.month(2018, 2)
@@ -90,3 +86,46 @@ print("snapshoot1 ", datetime.datetime.now().year)
 print("snapshoot2 ", time.strftime("%d/%m/%Y"))
 print("snapshoot3 ", time.strftime("%d/%m/%Y/%h"))
 print("snapshoot4 ", datetime.datetime.today().strftime("%A %d de %B de %Y"))
+
+print("\n\tsnap:\n")  # todo: STRUCT
+localtime = time.localtime(time.time())
+print("Current local time: ", localtime)
+
+print("\n\tsnap:\n")  # Todo: input time_frame calendar search engine management:
+enter = input('Input time frame: \n')
+split = list(enter)
+print(split)
+check_type = split[0]
+check = int(check_type)
+if check == 0:
+    check_in = split[1]
+else:
+    check_in = split[0] + split[1]
+check_type = split[2]
+check = int(check_type)
+if check == 0:
+    check_out = split[3]
+else:
+    check_out = split[2] + split[3]
+print("check in: ", check_in, " - check out: ", check_out)
+print("type check in: ", type(check_in))
+start = int(check_in)
+end = int(check_out)
+print("end - start: ", end - start)
+if end < start:
+    depart_month = 1
+    return_month = 2
+else:
+    depart_month = 1
+    return_month = 1
+print("month_vars: ", depart_month, " - ", return_month)
+# today = time.localtime()  # fixme CURRENT DAY
+# both localtime() and gmtime() returns an struct object, but but there is a time
+# difference between them, here we must use gmtime () to get the correct day :)
+today = time.gmtime()
+print(today.tm_year)
+print(today.tm_mon)
+print("day: ", today.tm_mday)
+print(today.tm_hour)
+t = today.tm_mday
+print("type of t: ", type(t))
