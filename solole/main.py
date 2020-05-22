4c6d2a53a9b936f12cd6023d88cba50500100bc4
josepmartorell <martorelljosep>
2020-05-22 16:45:33 +0200

    email

--- solole/main.py
+++ solole/main.py
@@ -1,6 +1,7 @@
 import time
 import operator
 from time import sleep
+from email import encoders
 from bs4 import BeautifulSoup
 from selenium import webdriver
 # todo: -> Check driver version periodically
@@ -15,6 +16,9 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
 from openpyxl.styles import PatternFill, Border, Side, Font
 from openpyxl.styles import Alignment, Protection
 from openpyxl import load_workbook
@@ -23,7 +27,9 @@ import target as t
 import data as d
 import datetime
 import requests
+import smtplib
 import shutil
+import ssl
 import os
 
 
@@ -469,9 +475,59 @@ class App:
 
         workbook.save(filepath)  # save file
 
+        spreadsheet = '//home/jmartorell/Booking/bookings/bookings.xlsx'
+        self.send_attachment(spreadsheet)
+
+    def send_attachment(self, file):
+        subject = "An email with attachment from Python"
+        body = "This is an email with attachment sent from Python"
+        sender_email = "jetro4100@gmail.com"
+        receiver_email = "martorelljosep@gmail.com"
+        # password = input("Type your password and press enter:")
+        password = 'ZXspectrum5128$}_'
+
+        # Create a multipart message and set headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email  # Recommended for mass emails
+
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        filename = file  # In same directory as script
+
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
+
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
+            print('Sending email ...')
+
 
 if __name__ == '__main__':
-    switch = 1
+    switch = 0
     if switch != 0:
         x = 0
         while x < 25:
